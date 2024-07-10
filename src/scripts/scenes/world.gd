5a22baa4ac565269082c87e43cd69356506140e1
gjpgabayeron <gjpgabayeron@gmail.com>
2024-07-10 15:28:46 +0800

    Initial Commit

--- src/scripts/scenes/world.gd
+++ src/scripts/scenes/world.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("This is a test");
+	print("World scene opened")
 	pass # Replace with function body.
 
 
