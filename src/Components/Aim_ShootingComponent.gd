eaa573e350deaf38d499a2417bc68c99063918bd
jhereed <jhereed@gmail.com>
2024-07-20 00:17:36 +0800

    Shooting
    I think PC gid aton ulubrahon kay ang UI di kaigo sa Mobile and ga obstruct sng view sng mga players
    @justCallMeJeg

--- src/Components/Aim_ShootingComponent.gd
+++ src/Components/Aim_ShootingComponent.gd
@@ -1,12 +1,29 @@
 class_name Aim_ShootingComponent
 extends Node
 
+@onready var main = get_tree().get_root().get_node("Main")#Creating bullet outside of the Player node to separate the bullet's position from the player's
+@onready var projectile = load("res://src/Player/bullet.tscn")
 @export var ACTOR : Node2D
+
+@export var shootingInterval: float
+var canFire = true
 
 func _process(delta):
 	var look_vector = (ACTOR.get_global_mouse_position() - ACTOR.global_position).normalized()
 	ACTOR.global_rotation = (atan2(look_vector.y, look_vector.x))+0.5 * PI
 	
+	if Input.is_action_pressed("attack") and canFire:
+		shoot()
+		canFire = false
+		await get_tree().create_timer(shootingInterval).timeout
+		canFire = true
 	
-	
+
+func shoot():
+	var instance = projectile.instantiate()
+	instance.dir = ACTOR.rotation
+	instance.instantiatePosition = ACTOR.global_position
+	instance.instantiateRotation = ACTOR.rotation
+	instance.zIndex = ACTOR.z_index - 1
+	main.add_child.call_deferred(instance)
 
--- src/Components/InputComponent.gd
+++ src/Components/InputComponent.gd
@@ -2,9 +2,12 @@ class_name InputComponent
 extends Node
 
 @export var MOVE_COMPONENT: MoveComponent
+@export var SHOOT_COMPONENT: Aim_ShootingComponent
 @export var MOVE_STATS: MovementStats
 
 func _input(event: InputEvent) -> void:
 	var left_right = Input.get_axis("ui_left", "ui_right")
 	var top_down = Input.get_axis("ui_up", "ui_down")
+	
+	
 	MOVE_COMPONENT.VELOCITY = Vector2(left_right * MOVE_STATS.SPEED, top_down * MOVE_STATS.SPEED)
