ae4bc180ce06ed2e9fcf3fcaf33248d880dbe12a
gjpgabayeron <gjpgabayeron@gmail.com>
2024-08-09 18:42:55 +0800

    Removed redundant code

--- src/Scripts/Scenes/SelectionScene/SelectionScene.gd
+++ src/Scripts/Scenes/SelectionScene/SelectionScene.gd
@@ -28,6 +28,3 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventKey and event.pressed:
 		if event.keycode == KEY_R: # REMOVE THIS BEFORE RELEASE
 			get_tree().reload_current_scene() # REMOVE THIS BEFORE RELEASE
-
-func setText() -> void:
-	pass
