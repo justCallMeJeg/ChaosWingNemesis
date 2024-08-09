c70a97bc33d8cbf8e09964faef7af585a84a17bc
jhereed <jhereed@gmail.com>
2024-08-09 09:04:54 +0800

    Invincibility frames

--- src/Scripts/Components/HealthComponent.gd
+++ src/Scripts/Components/HealthComponent.gd
@@ -2,6 +2,7 @@ extends Node
 
 @export var HITBOX_COMPONENT: Area2D
 @export var HEALTH: int = 3
+@export var CollisionPolygon : CollisionPolygon2D
 
 # Node Signals for external functions/components
 signal healthDepleted()
@@ -17,9 +18,13 @@ func _process(delta):
 	if hurt.size() > 0:
 		HEALTH -= 1 * hurt.size() * delta 
 		print(HEALTH)
+		CollisionPolygon.set_deferred("disabled",true)
+		await get_tree().create_timer(3).timeout
+		CollisionPolygon.set_deferred("disabled",false)
+	
 	if HEALTH <= 0:
 		healthDepleted.emit()
-		get_parent().queue_free()#Still dont know how to do invincibility frames
+		get_parent().queue_free()
 
 
 
