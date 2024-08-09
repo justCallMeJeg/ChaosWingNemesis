85a4e949ddc91b75cc8dcee80fee5f5daeb2c0f7
jhereed <jhereed@gmail.com>
2024-08-09 21:00:16 +0800

    Update SMOL.gd

--- src/Scripts/Augments/SMOL.gd
+++ src/Scripts/Augments/SMOL.gd
@@ -6,6 +6,7 @@ extends Node
 func _ready():
 	BASESHIP.get_node("ShipBody").scale *= Vector2(0.5,0.5)
 	BASESHIP.get_node("ShipFlame").scale *= Vector2(0.5,0.5)
+	BASESHIP.get_node("ShipFlame").position += Vector2(0,-5.5)
 	BASESHIP.get_node("Collision").scale *= Vector2(0.5,0.5)
 	BASESHIP.get_node("HitboxComponent").get_node("CollisionPolygon2D").scale *= Vector2(0.5,0.5) 
 
