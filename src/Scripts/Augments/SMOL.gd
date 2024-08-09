93f533768b172dc19bc506efb279f34091d1cc3b
jhereed <jhereed@gmail.com>
2024-08-09 19:41:25 +0800

    SMOL Augment
    Need minor tweaks with the position of the shipflame

--- src/Scripts/Augments/SMOL.gd
+++ src/Scripts/Augments/SMOL.gd
@@ -1,9 +1,13 @@
 extends Node
 
+@onready var BASESHIP =  $"/root/Main/BaseShip"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	BASESHIP.get_node("ShipBody").scale *= Vector2(0.5,0.5)
+	BASESHIP.get_node("ShipFlame").scale *= Vector2(0.5,0.5)
+	BASESHIP.get_node("Collision").scale *= Vector2(0.5,0.5)
+	BASESHIP.get_node("HitboxComponent").get_node("CollisionPolygon2D").scale *= Vector2(0.5,0.5) 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
