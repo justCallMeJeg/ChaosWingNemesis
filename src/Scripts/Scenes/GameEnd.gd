69d0859560b9d488de460d509ce1b70b70590e41
Rover <qsxdrtgbnmjuy@protonmail.ch>
2024-08-10 15:13:22 +0800

    Added quit shortcut

--- src/Scripts/Scenes/GameEnd.gd
+++ src/Scripts/Scenes/GameEnd.gd
@@ -5,3 +5,8 @@ func _ready():
 	animation_player.play("anim")
 	await animation_player.animation_finished
 	SceneTransition.loadScene("res://src/Scenes/MainMenu.tscn")
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_ESCAPE: # REMOVE THIS BEFORE RELEASE
+			SceneTransition.loadScene("res://src/Scenes/MainMenu.tscn")
